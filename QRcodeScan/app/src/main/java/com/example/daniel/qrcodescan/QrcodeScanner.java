7311ed6e4d6757bf3fab351aa480c370f577d875
Hazel Lin <hazaellyuanh@gmail.com>
2018-07-18 14:47:00 +0800

    test

---
+++ QRcodeScan/app/src/main/java/com/example/daniel/qrcodescan/QrcodeScanner.java
@@ -0,0 +1,191 @@
+package com.example.daniel.qrcodescan;
+
+import android.content.DialogInterface;
+import android.content.pm.ActivityInfo;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import net.sourceforge.zbar.Config;
+import net.sourceforge.zbar.Image;
+import net.sourceforge.zbar.ImageScanner;
+import net.sourceforge.zbar.Symbol;
+import net.sourceforge.zbar.SymbolSet;
+
+public class QrcodeScanner extends AppCompatActivity {
+
+    private Camera mCamera;
+    private CameraPreview mPreview;
+    private Handler autoFocusHandler;
+
+    private Button scanButton;
+    private ImageScanner scanner;
+
+    private boolean barcodeScanned = false;
+    private boolean previewing = true;
+
+    static {
+        System.loadLibrary("iconv");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.qrcode_scanner);
+
+        initControls();
+    }
+
+    private void initControls() {
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        autoFocusHandler = new Handler();
+        mCamera = getCameraInstance();
+
+        // Instance barcode scanner
+        scanner = new ImageScanner();
+        scanner.setConfig(0, Config.X_DENSITY, 3);
+        scanner.setConfig(0, Config.Y_DENSITY, 3);
+
+        mPreview = new CameraPreview(QrcodeScanner.this, mCamera, previewCb,
+                autoFocusCB);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.cameraPreview);
+        preview.addView(mPreview);
+
+        scanButton = (Button) findViewById(R.id.ScanButton);
+
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (barcodeScanned) {
+                    barcodeScanned = false;
+                    mCamera.setPreviewCallback(previewCb);
+                    mCamera.startPreview();
+                    previewing = true;
+                    mCamera.autoFocus(autoFocusCB);
+                }
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            releaseCamera();
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+
+    /**
+     * A safe way to get an instance of the Camera object.
+     */
+    public static Camera getCameraInstance() {
+        Camera c = null;
+        try {
+            c = Camera.open();
+        } catch (Exception e) {
+        }
+        return c;
+    }
+
+    private void releaseCamera() {
+        if (mCamera != null) {
+            previewing = false;
+            mCamera.setPreviewCallback(null);
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+
+    private Runnable doAutoFocus = new Runnable() {
+        public void run() {
+            if (previewing)
+                mCamera.autoFocus(autoFocusCB);
+        }
+    };
+
+    Camera.PreviewCallback previewCb = new Camera.PreviewCallback() {
+        public void onPreviewFrame(byte[] data, Camera camera) {
+            Camera.Parameters parameters = camera.getParameters();
+            Camera.Size size = parameters.getPreviewSize();
+
+            Image barcode = new Image(size.width, size.height, "Y800");
+            barcode.setData(data);
+
+            int result = scanner.scanImage(barcode);
+
+            if (result != 0) {
+                previewing = false;
+                mCamera.setPreviewCallback(null);
+                mCamera.stopPreview();
+
+                SymbolSet syms = scanner.getResults();
+                for (Symbol sym : syms) {
+
+                    Log.i("<<<<<<Asset Code>>>>> ",
+                            "<<<<Bar Code>>> " + sym.getData());
+
+                    String scanResult = sym.getData().trim();
+
+
+                    //mSym.getType()方法可以获取扫描的类型，ZBar支持多种扫描类型,这里实现了条形码、二维码、ISBN码的识别
+                    if (sym.getType() == Symbol.CODE128 || sym.getType() == Symbol.QRCODE ||
+                            sym.getType() == Symbol.CODABAR || sym.getType() == Symbol.ISBN10 ||
+                            sym.getType() == Symbol.ISBN13|| sym.getType()==Symbol.DATABAR ||
+                            sym.getType()==Symbol.DATABAR_EXP || sym.getType()==Symbol.I25)
+
+                    {
+                        barcodeScanned = true;
+                        showAlertDialog(scanResult);
+                        Toast.makeText(QrcodeScanner.this, scanResult,
+                                Toast.LENGTH_SHORT).show();
+                    }
+
+                    else
+                    {
+                        //否则继续扫描
+                        barcodeScanned = false;
+                        mCamera.setPreviewCallback(previewCb);
+                        mCamera.startPreview();
+                        previewing = true;
+                        mCamera.autoFocus(autoFocusCB);
+                    }
+
+                }
+            }
+        }
+    };
+
+    // Mimic continuous auto-focusing
+    Camera.AutoFocusCallback autoFocusCB = new Camera.AutoFocusCallback() {
+        public void onAutoFocus(boolean success, Camera camera) {
+            autoFocusHandler.postDelayed(doAutoFocus, 1000);
+        }
+    };
+
+    private void showAlertDialog(String message) {
+
+        new AlertDialog.Builder(this)
+                .setTitle(getResources().getString(R.string.app_name))
+                .setCancelable(false)
+                .setMessage(message)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+
+                .show();
+    }
+
+
+}
